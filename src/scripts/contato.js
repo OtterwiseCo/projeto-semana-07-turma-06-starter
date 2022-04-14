fc3b823abf908139d3f497c882e9c36fc3c334eb
Aline Nunes <linna2vezes@gmail.com>
2022-04-13 23:00:24 -0300

    finalizado

--- src/scripts/contato.js
+++ src/scripts/contato.js
@@ -1,111 +1,18 @@
-function onSubmit (event) {
-  event.preventDefault()
-  
-  const name= document.querySelector('name').value
-  const email= document.querySelector('email').value
-  const comment= document.querySelector('comment').value
-
-  console.log(name, email,comment)
-
-//  enviar p API
-  const data = {
-  name: name,
-  email: email,
-  body: comment,
-  }
 
 
+window.onload = () => {
+const form = document.querySelector("#form");
+form.addEventListener("submit", event => {
+  event.preventDefault();
+const formData = new FormData(form)
+const name = formData.get("name")
+const email = formData.get("email")
+const comment = formData.get("comment")
+const data = {nome: name , email: email, comentario: comment}
+// const students = { nome: "Mari" , age: "21"}
 console.log(data)
-console.log(JSON.stringify(data))
+alert("dados enviados")
+event.target.reset()
+})
 
 }
-// const url= 'https://jsonplaceholder.typicode.com/comments'
-
-// fetch( url, {
-//   method: 'GET',
-//   body: JSON.stringify({
-//     title: 'foo',
-//     body: 'bar',
-//     userId: 1,
-//   }),
-//   headers: {
-//     'Content-type': 'application/json; charset=UTF-8',
-//   },
-// })
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
-
-
-window.onload = () => { 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", onSubmit)
-
-}
-
-
-
-
-
-//  window.onload = () => { 
-//   const button = document.querySelector("button");
-//     button.addEventListener("click", (event) => {
-//       const form = document.querySelector('fieldset')
-//     return form
-//     })
-//     console.log(form)
-//  }
-
-
-// function onclick() {
-//   console.log('enviado')  
-// }
- 
-//  window.onload = () => { 
-//       const button = document.querySelector('button');
-//       button.addEventListener('click', onclick )
-//     }
-
-
- 
-
-
-    // window.onload = () => { 
-    //   const button = document.querySelector("button");
-    //    button.addEventListener("click", (event) => {
-    //     const form = document.getElementsByName("name");
-    //     const email = document.getElementsByName("email");
-    //     const message = document.getElementsByName('message');
-    //    console.log(name)
-    //    console.log(email)  
-    //    console.log(message)    
-    //    })
-    // }
-
-    // function myFunction(a, b) {
-    //   document.getElementById("demo").innerHTML = a * b;
-    // }  
-  // window.onload = () => { 
- 
-
-  // //  const button = document.querySelector("button");
-   
-  // //  if (name !== blank & email !== blank) {
-
-  //   button.addEventListener("click", (event) => {
-  //   //  const blank = " ";
-  //  const name = document.getElementsByName("name");
-  //  const email = document.getElementsByName("email");
-  //  const message = document.getElementsByName('message');
-   
-    
-  //  console.log(name + email + message)
-  //   //    return "ola";
-  //   //      }
-
-  //   // if (name == blank || email == blank || message == blank) {
-    
-  //   }
-    
-  //   );
-   
-  //  };
